6f3dda5e0509154d1dfce4e8a1f11c08fa8f5659
Daniel Lozano <d.lozano343@uniandes.edu.co>
2019-06-13 12:10:55 -0500

    contour with animation

--- VISUALIZE/contour.py
+++ VISUALIZE/contour.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.animation as animation
+import animatplot as amp
 
-
-x=np.linspace(-2.5,2.5,100)
+x=np.linspace(-2.5,2.5,500)
 
 hh,l= np.meshgrid(x, x)
 
@@ -32,4 +33,25 @@ plt.ylabel("$ (00l) $")
 plt.title("$ J_{3a} $  and  $ J_{3b} $ ")
 plt.show()
 
+bt=np.linspace(0,2,100)
+hh,l,BT= np.meshgrid(x, x, bt)
+cmap_label="jet"
+
+
+def Z_J3(hh,l,BT3):
+    return 2*(1+BT3)*(np.cos(np.pi*(l+hh))+np.cos(np.pi*(l-hh)))+2*BT3*np.cos(2*np.pi*hh)
+
+pcolormesh_data =Z_J3(hh,l,BT)
+pcolormesh_block = amp.blocks.Pcolormesh(hh[:,:,0], l[:,:,0], pcolormesh_data,
+                                         t_axis=2, vmin=-1, vmax=1,cmap=cmap_label)
+plt.colorbar(pcolormesh_block.quad)
+plt.xlabel("$ (hh0) $")
+plt.ylabel("$ (00l) $")
+timeline = amp.Timeline(bt, fps=10)
+
+# now to contruct the animation
+anim = amp.Animation([pcolormesh_block], timeline)
+anim.controls()
+
+plt.show()
 
